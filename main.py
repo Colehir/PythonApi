629397949283cdb325a91028127a1299a5cd7886
Colehir <cole.hirapara@gmail.com>
2017-07-19 16:26:53 -0400

    and nessisary methods

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+import os
 from flask import request
 from flask import Flask
 import json
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+class Snake:
+    def __init__(self):
+        self.list = []
+        self.food = {}
+
+snake = Snake()
+
+@app.route('/snake', methods=['GET'])
 def getSnake():
-    return json.dumps({ "x": 1 })
+    return json.dumps(snake.list)
 
-@app.route('/help', methods=['GET'])
-def getHelp():
-    return json.dumps({ "x": 1 })
+@app.route('/food', methods=['GET'])
+def getFood():
+    return json.dumps(snake.food)
 
+@app.route('/snake', methods=['POST'])
+def updateSnake():
+    newList = request.json['list']
+    snake.list = newList
+
+@app.route('/food', methods=['POST'])
+def updateFood():
+    newFood = request.json['food']
+    snake.food = newFood
+
+port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
     print("starting")
-    app.run()
+    app.run(host='0.0.0.0', port=int(port))
